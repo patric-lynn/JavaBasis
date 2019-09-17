b85d48d1e9de2d99a99b1e384cbcbec2a0af982b
patric-lynn <358812977@163.com>
2019-09-17 20:24:19 +0800

    1.浮点数直接加的错误显示验证。（3.8.4节）

---
+++ src/main/java/basic.java
@@ -0,0 +1,11 @@
+public class basic {
+
+    public static void main(String[] args) {
+        for(double i=1; i!=100; i+=0.1){
+            System.out.println("This is "+ i +" time try");
+        }
+        return;
+    }
+
+}
+

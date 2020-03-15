4ecb8babe867ed2f2fae255a16568cb3754807ea
lynn <358812977@163.com>
2020-03-15 21:58:47 +0800

    新增 TreeRelatedQuestions

---
+++ src/main/java/leetCode/TreeRelatedQuestions.java
@@ -0,0 +1,152 @@
+package leetCode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * Description
+ *
+ * @author Lynn-zd
+ * @date Created on 2020/3/15 18:14
+ */
+public class TreeRelatedQuestions {
+    private static int count = 0;
+
+    /**
+     * 二叉树定义
+     */
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+    /**
+     * 543. 二叉树的直径
+     * 给定一棵二叉树，你需要计算它的直径长度。一棵二叉树的直径长度是任意两个结点路径长度中的最大值。这条路径可能穿过根结点。
+     * <p>
+     * 示例 :
+     * 给定二叉树
+     * <p>
+     * 1
+     * / \
+     * 2   3
+     * / \
+     * 4   5
+     * 返回 3, 它的长度是路径 [4,2,1,3] 或者 [5,2,1,3]。
+     * <p>
+     * 注意：两结点之间的路径长度是以它们之间边的数目表示。
+     *
+     * @param root
+     * @return count
+     */
+    public static int diameterOfBinaryTree(TreeNode root) {
+        dfs(root);
+        return count;
+    }
+
+    public static int dfs(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int left = dfs(root.left);
+        int right = dfs(root.right);
+        count = Math.max(count, left + right);
+        return Math.max(left, right) + 1;
+    }
+
+
+    /**
+     * 104. 二叉树的最大深度
+     * 给定一个二叉树，找出其最大深度。
+     * <p>
+     * 二叉树的深度为根节点到最远叶子节点的最长路径上的节点数。
+     * <p>
+     * 说明: 叶子节点是指没有子节点的节点。
+     * <p>
+     * 示例：
+     * 给定二叉树 [3,9,20,null,null,15,7]，
+     * <p>
+     * 3
+     * / \
+     * 9  20
+     * /  \
+     * 15   7
+     * 返回它的最大深度 3 。
+     *
+     * @param root
+     * @return count
+     */
+    public int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+        int left = maxDepth(root.left) + 1;
+        int right = maxDepth(root.right) + 1;
+        count = (left > right) ? left : right;
+        return count;
+    }
+
+
+    /**
+     * 101. 对称二叉树
+     * 给定一个二叉树，检查它是否是镜像对称的。
+     * <p>
+     * 例如，二叉树 [1,2,2,3,4,4,3] 是对称的。
+     * <p>
+     * 1
+     * / \
+     * 2   2
+     * / \ / \
+     * 3  4 4  3
+     * 但是下面这个 [1,2,2,null,3,null,3] 则不是镜像对称的:
+     * <p>
+     * 1
+     * / \
+     * 2   2
+     * \   \
+     * 3    3
+     * 说明:
+     * <p>
+     * 如果你可以运用递归和迭代两种方法解决这个问题，会很加分。
+     * <p>
+     * 解法一：递归法
+     *
+     * @param root
+     * @return
+     */
+    public boolean isSymmetric(TreeNode root) {
+        return isMirror(root, root);
+    }
+
+    public boolean isMirror(TreeNode root1, TreeNode root2) {
+        if (root1 == null && root2 == null) return true;
+        if (root1 == null || root2 == null) return false;
+        return (root1.val == root2.val) && isMirror(root1.left, root2.right) && isMirror(root1.right, root2.left);
+    }
+
+
+    public boolean isSymmetric2(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.offer(root);
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            TreeNode left = queue.poll();
+            TreeNode right = queue.poll();
+            if (left == null && right == null) continue;
+            if (left == null || right == null) return false;
+            if (left.val != right.val) return false;
+            queue.offer(left.left);
+            queue.offer(right.right);
+            queue.offer(left.right);
+            queue.offer(right.left);
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}

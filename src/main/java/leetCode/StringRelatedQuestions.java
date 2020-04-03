5e0d755d4bf688a09234e7c6c8b157f7c4614900
lynn <358812977@163.com>
2020-04-03 23:56:11 +0800

    新增代码

--- src/main/java/leetCode/StringRelatedQuestions.java
+++ src/main/java/leetCode/StringRelatedQuestions.java
@@ -255,6 +255,88 @@ class StringRelatedQuestions {
         }
         return dp[0][0];
     }
+
+
+    /**
+     * 14. 最长公共前缀
+     * 编写一个函数来查找字符串数组中的最长公共前缀。
+     *
+     * 如果不存在公共前缀，返回空字符串 ""。
+     *
+     * 示例 1:
+     *
+     * 输入: ["flower","flow","flight"]
+     * 输出: "fl"
+     * 示例 2:
+     *
+     * 输入: ["dog","racecar","car"]
+     * 输出: ""
+     * 解释: 输入不存在公共前缀。
+     * 说明:
+     *
+     * 所有输入只包含小写字母 a-z 。
+     *
+     * 解法一：
+     * 当字符串数组长度为 0 时则公共前缀为空，直接返回
+     * 令最长公共前缀 ans 的值为第一个字符串，进行初始化
+     * 遍历后面的字符串，依次将其与 ans 进行比较，两两找出公共前缀，最终结果即为最长公共前缀
+     * 如果查找过程中出现了 ans 为空的情况，则公共前缀不存在直接返回
+     * @param strs
+     * @return
+     */
+    public String longestCommonPrefix(String[] strs) {
+        if(strs.length == 0)
+            return "";
+        String ans = strs[0];
+        for(int i =1;i<strs.length;i++) {
+            int j=0;
+            for(;j<ans.length() && j < strs[i].length();j++) {
+                if(ans.charAt(j) != strs[i].charAt(j))
+                    break;
+            }
+            ans = ans.substring(0, j);
+            if(ans.equals(""))
+                return ans;
+        }
+        return ans;
+    }
+    /**
+     * 解法二：多种情况考虑
+     * @param strs
+     * @return
+     */
+    public String longestCommonPrefix2(String[] strs) {
+        if (strs.length == 0) {
+            return "";
+        }
+        if (strs.length == 1) {
+            return strs[0];
+        }
+        String min = strs[0] ;
+        for (int i = 0; i < strs.length; i++) {
+            if (strs[i].isEmpty()) {
+                return "";
+            }
+            if (strs[0].charAt(0) != strs[i].charAt(0)) {
+                return "";
+            }
+            if (strs[i].length() <= min.length()) {
+                min = strs[i];
+            }
+        }
+        for (int i = 0; i < strs.length; i++) {
+            if (min.equals(strs[i])) {
+                continue;
+            }
+            for (int j = min.length()-1; j > 0 ; j--) {
+                if (min.charAt(j) != strs[i].charAt(j)) {
+                    min = min.substring(0, j);
+                }
+            }
+        }
+        return min;
+    }
+
     public static void main(String[] args) {
 
     }

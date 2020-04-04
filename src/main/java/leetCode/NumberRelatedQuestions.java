77eacf8331d2614eb2af3547e607006ea1e990cd
lynn <358812977@163.com>
2020-04-04 18:32:42 +0800

    新增代码

--- src/main/java/leetCode/NumberRelatedQuestions.java
+++ src/main/java/leetCode/NumberRelatedQuestions.java
@@ -276,6 +276,37 @@ public class NumberRelatedQuestions {
         return ans;
     }
 
+
+    /**
+     * 16. 最接近的三数之和
+     * 给定一个包括 n 个整数的数组 nums 和 一个目标值 target。找出 nums 中的三个整数，
+     * 使得它们的和与 target 最接近。返回这三个数的和。假定每组输入只存在唯一答案。
+     * 例如，给定数组 nums = [-1，2，1，-4], 和 target = 1.
+     * 与 target 最接近的三个数的和为 2. (-1 + 2 + 1 = 2).
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+        int result = nums[0]+nums[1]+nums[2];
+        for(int i=0; i<nums.length-2; i++){
+            int start = i+1, end=nums.length-1;
+            while(start<end){
+                int sum = nums[i]+nums[start]+nums[end];
+                if(Math.abs(target-sum) < Math.abs(target-result)) result = sum;
+                if(sum>target){
+                    end--;
+                } else if(sum<target){
+                    start++;
+                }else{
+                    return result;
+                }
+            }
+        }
+        return result;
+    }
     public static void main(String[] args) {
     }
 

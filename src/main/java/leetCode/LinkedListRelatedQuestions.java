7adb2da169d9fc5339dcaa40e103bc95cdf38277
lynn <358812977@163.com>
2020-04-04 23:09:18 +0800

    新增代码

--- src/main/java/leetCode/LinkedListRelatedQuestions.java
+++ src/main/java/leetCode/LinkedListRelatedQuestions.java
@@ -54,6 +54,44 @@ public class LinkedListRelatedQuestions {
         return PlusNode.next;
     }
 
+
+    /**
+     * 19. 删除链表的倒数第N个节点
+     * 给定一个链表，删除链表的倒数第 n 个节点，并且返回链表的头结点。
+     *
+     * 示例：
+     * 给定一个链表: 1->2->3->4->5, 和 n = 2.
+     * 当删除了倒数第二个节点后，链表变为 1->2->3->5.
+     *
+     * 解法一：整体思路是让前面的指针先移动n步，之后前后指针共同移动直到前面的指针到尾部为止
+     * 首先设立预先指针 pre，预先指针是一个小技巧，在第2题中进行了讲解
+     * 设预先指针 pre 的下一个节点指向 head，设前指针为 start，后指针为 end，二者都等于 pre
+     * start 先向前移动n步，之后 start 和 end 共同向前移动，此时二者的距离为 n，
+     * 当 start 到尾部时，end 的位置恰好为倒数第 n 个节点，因为要删除该节点，
+     * 所以要移动到该节点的前一个才能删除，所以循环结束条件为 start.next != null
+     * 删除后返回 pre.next，为什么不直接返回 head 呢，因为 head 有可能是被删掉的点
+     *
+     * @param head
+     * @param n
+     * @return
+     */
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode result = new ListNode(0);
+        result.next=head;
+        ListNode start=result, post = result;
+        int count = 0;
+        while(count<n){
+            count++;
+            start=start.next;
+        }
+        while(start.next!=null){
+            start=start.next;
+            post=post.next;
+        }
+        post.next = post.next.next;
+        return result.next;
+    }
+
     public static void main(String[] args) {
 
     }

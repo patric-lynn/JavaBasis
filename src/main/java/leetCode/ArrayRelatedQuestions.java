3a6630f7281e16a5a18a91dd00a21cef58e22a21
lynn <358812977@163.com>
2020-04-07 14:58:49 +0800

    新增代码

--- src/main/java/leetCode/ArrayRelatedQuestions.java
+++ src/main/java/leetCode/ArrayRelatedQuestions.java
@@ -2,7 +2,10 @@ package leetCode;
 
 /**
  * Description
- *
+ * 26. 删除排序数组中的重复项
+ * 27. 移除元素
+ * 34. 在排序数组中查找元素的第一个和最后一个位置
+ * 35. 二分查找搜索插入位置
  * @author Lynn-zd
  * @date Created on 2020/4/7 13:54
  */
@@ -32,6 +35,103 @@ public class ArrayRelatedQuestions {
             }
         }
         return i + 1;
+    }
+
+    /**
+     * 27. 移除元素
+     * 给你一个数组 nums 和一个值 val，你需要 原地 移除所有数值等于 val 的元素，并返回移除后数组的新长度。
+     * 不要使用额外的数组空间，你必须仅使用 O(1) 额外空间并 原地 修改输入数组。
+     * 元素的顺序可以改变。你不需要考虑数组中超出新长度后面的元素。
+     * 示例 1:
+     * 给定 nums = [3,2,2,3], val = 3,
+     * 函数应该返回新的长度 2, 并且 nums 中的前两个元素均为 2。
+     * 你不需要考虑数组中超出新长度后面的元素。
+     *
+     * @param nums
+     * @param val
+     * @return
+     */
+    public int removeElement(int[] nums, int val) {
+        int i = 0;
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] != val) {
+                nums[i] = nums[j];
+                i++;
+            }
+        }
+        return i;
+    }
+
+    /**
+     * 34. 在排序数组中查找元素的第一个和最后一个位置
+     * 给定一个按照升序排列的整数数组 nums，和一个目标值 target。找出给定目标值在数组中的开始位置和结束位置。
+     * 你的算法时间复杂度必须是 O(log n) 级别。
+     * 如果数组中不存在目标值，返回 [-1, -1]。
+     * <p>
+     * 示例 1:
+     * <p>
+     * 输入: nums = [5,7,7,8,8,10], target = 8
+     * 输出: [3,4]
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int[] searchRange(int[] nums, int target) {
+        int[] targetRange = {-1, -1};
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target) {
+                targetRange[0] = i;
+                break;
+            }
+        }
+
+        if (targetRange[0] == -1) {
+            return targetRange;
+        }
+
+        for (int j = nums.length - 1; j >= 0; j--) {
+            if (nums[j] == target) {
+                targetRange[1] = j;
+                break;
+            }
+        }
+
+        return targetRange;
+    }
+
+    /**
+     * 35. 二分查找搜索插入位置
+     * 给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+     * 你可以假设数组中无重复元素。
+     *
+     * 示例 1:
+     *
+     * 输入: [1,3,5,6], 5
+     * 输出: 2
+     * 示例 2:
+     *
+     * 输入: [1,3,5,6], 2
+     * 输出: 1
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int searchInsert(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while(left <= right) {
+            int mid = (left + right) / 2;
+            if(nums[mid] == target) {
+                return mid;
+            } else if(nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return left;
     }
     public static void main(String[] args) {
 

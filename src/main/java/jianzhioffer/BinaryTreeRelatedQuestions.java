c68237df311acb4ae6909c015370de3b555bc703
lynn <358812977@163.com>
2020-06-14 23:13:36 +0800

    新增代码

--- src/main/java/jianzhioffer/BinaryTreeRelatedQuestions.java
+++ src/main/java/jianzhioffer/BinaryTreeRelatedQuestions.java
@@ -16,12 +16,11 @@ public class BinaryTreeRelatedQuestions {
         int val;
         TreeNode left;
         TreeNode right;
+
         TreeNode(int x) {
             val = x;
         }
     }
-
-
 
 
     /**
@@ -31,29 +30,32 @@ public class BinaryTreeRelatedQuestions {
      * 前序遍历 preorder = [3,9,20,15,7]
      * 中序遍历 inorder = [9,3,15,20,7]
      * 返回如下的二叉树：
-     *     3
-     *    / \
-     *   9  20
-     *     /  \
-     *    15   7
+     * 3
+     * / \
+     * 9  20
+     * /  \
+     * 15   7
+     *
      * @param preorder
      * @param inorder
      * @return
      */
-    HashMap<Integer,Integer> map = new HashMap<>();
+    HashMap<Integer, Integer> map = new HashMap<>();
     int[] pre;
+
     public TreeNode buildTree(int[] preorder, int[] inorder) {
         pre = preorder;
-        for (int i=0; i< pre.length; i++)
-            map.put(inorder[i],i);
-        return recursive(0,0,inorder.length-1);
+        for (int i = 0; i < pre.length; i++)
+            map.put(inorder[i], i);
+        return recursive(0, 0, inorder.length - 1);
     }
-    TreeNode recursive(int pre_root,int in_left,int in_right){
-        if(in_left>in_right) return null;
+
+    TreeNode recursive(int pre_root, int in_left, int in_right) {
+        if (in_left > in_right) return null;
         TreeNode root = new TreeNode(pre[pre_root]);
         int i = map.get(pre[pre_root]);
-        root.left = recursive(pre_root+1,in_left,i-1);
-        root.right = recursive(pre_root+i+1-in_left,i+1,in_right);
+        root.left = recursive(pre_root + 1, in_left, i - 1);
+        root.right = recursive(pre_root + i + 1 - in_left, i + 1, in_right);
         return root;
     }
 

837fc915bf7b08e51cb5322c6f01d84488291ad7
lynn <358812977@163.com>
2020-06-27 00:52:10 +0800

    新增代码

--- src/main/java/jianzhioffer/ArrayRelatedQuestions.java
+++ src/main/java/jianzhioffer/ArrayRelatedQuestions.java
@@ -5,7 +5,7 @@ import java.util.HashSet;
 import java.util.List;
 
 /**
- * 面试题03. 找到数组中重复的数字
+ * 面试题03. 找到数组中重复的数字:请找出一个长度为 n 的数组 nums 数组中任意一个重复的数字。[HashSet]
  * 面试题04. 二维数组中查找某一数
  * 面试题11. 旋转数组的最小数字
  * 面试题12. 矩阵中的路径
@@ -16,17 +16,43 @@ import java.util.List;
  */
 public class ArrayRelatedQuestions {
     /**
+     * 面试题03. 数组中重复的数字
+     * 在一个长度为 n 的数组 nums 里的所有数字都在 0～n-1 的范围内。
+     * 数组中某些数字是重复的，但不知道有几个数字重复了，也不知道每个数字重复了几次。
+     * 请找出数组中任意一个重复的数字。
+     * 示例 1：
+     * 输入：
+     * [2, 3, 1, 0, 2, 5, 3]
+     * 输出：2 或 3
+     *
+     * @param nums
+     * @return
+     */
+    public int findRepeatNumber(int[] nums) {
+        int result = -1;
+        HashSet<Integer> set = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            Boolean flag = set.add(nums[i]);
+            if (!flag) {
+                result = nums[i];
+                break;
+            }
+        }
+        return result;
+    }
+
+    /**
      * 面试题04. 二维数组中的查找
      * 在一个二维数组中（每个一维数组的长度相同），每一行都按照从左到右递增的顺序排序，每一列都按照从上到下递增的顺序排序。
      * 请完成一个函数，输入这样的一个二维数组和一个整数，判断数组中是否含有该整数。
-     * <p>
-     * 暴力法，遍历数组
+     *
+     * 暴力法，遍历数组[不推荐]
      *
      * @param target
      * @param array
      * @return
      */
-    public static boolean Find(int target, int[][] array) {
+    public static boolean findNumberIn2DArray(int target, int[][] array) {
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array[0].length; j++) {
                 if (array[i][j] == target) {
@@ -40,7 +66,7 @@ public class ArrayRelatedQuestions {
     /**
      * 从左下角找
      */
-    public static boolean Find2(int target, int[][] array) {
+    public static boolean findNumberIn2DArray2(int target, int[][] array) {
         int rows = array.length;
         if (rows == 0) {
             return false;
@@ -51,6 +77,7 @@ public class ArrayRelatedQuestions {
         }
         int row = rows - 1;
         int col = 0;
+        //
         while (row >= 0 && col < cols) {
             if (array[row][col] < target) {
                 col++;
@@ -68,7 +95,7 @@ public class ArrayRelatedQuestions {
      *
      * @param
      */
-    public static boolean Find3(int target, int[][] array) {
+    public static boolean findNumberIn2DArray3(int target, int[][] array) {
         int rows = array.length;
         if (rows == 0) {
             return false;
@@ -92,29 +119,6 @@ public class ArrayRelatedQuestions {
         return false;
     }
 
-    /**
-     * 面试题03. 数组中重复的数字
-     * 在一个长度为 n 的数组 nums 里的所有数字都在 0～n-1 的范围内。数组中某些数字是重复的，但不知道有几个数字重复了，也不知道每个数字重复了几次。请找出数组中任意一个重复的数字。
-     * 示例 1：
-     * 输入：
-     * [2, 3, 1, 0, 2, 5, 3]
-     * 输出：2 或 3
-     *
-     * @param nums
-     * @return
-     */
-    public int findRepeatNumber(int[] nums) {
-        int result = -1;
-        HashSet<Integer> set = new HashSet<>();
-        for (int i = 0; i < nums.length; i++) {
-            Boolean flag = set.add(nums[i]);
-            if (!flag) {
-                result = nums[i];
-                break;
-            }
-        }
-        return result;
-    }
 
     /**
      * 面试题11. 旋转数组的最小数字

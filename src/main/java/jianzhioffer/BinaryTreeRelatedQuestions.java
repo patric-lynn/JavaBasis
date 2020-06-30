436218d667443398f4851bdf144123893546c952
davisdzhang <davisdzhang@git.code.oa.com>
2020-06-30 23:36:30 +0800

    更新代码: BinaryTreeRelatedQuestions

--- src/main/java/jianzhioffer/BinaryTreeRelatedQuestions.java
+++ src/main/java/jianzhioffer/BinaryTreeRelatedQuestions.java
@@ -4,14 +4,27 @@ import java.util.HashMap;
 
 /**
  * Description
- *
+ * 面试题07. 重建二叉树：输入某二叉树的前序遍历和中序遍历的结果，请重建该二叉树。[递归]
  * @author Lynn-zd
  * @date Created on 2020/4/7 23:49
  */
 public class BinaryTreeRelatedQuestions {
     /**
+     * TreeNode定义
+     */
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+    /**
      * 面试题07. 重建二叉树
-     * 输入某二叉树的前序遍历和中序遍历的结果，请重建该二叉树。假设输入的前序遍历和中序遍历的结果中都不含重复的数字。
+     * 输入某二叉树的前序遍历和中序遍历的结果，请重建该二叉树。
+     * 假设输入的前序遍历和中序遍历的结果中都不含重复的数字。
      * 例如，给出
      * 前序遍历 preorder = [3,9,20,15,7]
      * 中序遍历 inorder = [9,3,15,20,7]
@@ -44,18 +57,4 @@ public class BinaryTreeRelatedQuestions {
         root.right = recursive(pre_root + i + 1 - in_left, i + 1, in_right);
         return root;
     }
-
-    /**
-     * TreeNode定义
-     */
-    class TreeNode {
-        int val;
-        TreeNode left;
-        TreeNode right;
-
-        TreeNode(int x) {
-            val = x;
-        }
-    }
-
 }

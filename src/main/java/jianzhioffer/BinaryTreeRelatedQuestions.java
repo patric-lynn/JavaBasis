ba468deb203b1726f216f309e854fdaaf744a584
davisdzhang <davisdzhang@git.code.oa.com>
2020-07-07 11:58:02 +0800

    更新代码: BinaryTreeRelatedQuestions

--- src/main/java/jianzhioffer/BinaryTreeRelatedQuestions.java
+++ src/main/java/jianzhioffer/BinaryTreeRelatedQuestions.java
@@ -8,6 +8,7 @@ import java.util.Stack;
  * 剑指 Offer 07. 重建二叉树: 输入某二叉树的前序遍历和中序遍历的结果，请重建该二叉树。[递归]
  * 剑指 Offer 26. 树的子结构: 输入两棵二叉树A和B，判断B是不是A的子结构。[递归]
  * 剑指 Offer 27. 二叉树的镜像: 请完成一个函数，输入一个二叉树，该函数输出它的镜像。[递归与非递归]
+ *
  * @author Lynn-zd
  * @date Created on 2020/4/7 23:49
  */
@@ -24,6 +25,7 @@ public class BinaryTreeRelatedQuestions {
             val = x;
         }
     }
+
     /**
      * 面试题07. 重建二叉树
      * 输入某二叉树的前序遍历和中序遍历的结果，请重建该二叉树。
@@ -78,9 +80,10 @@ public class BinaryTreeRelatedQuestions {
     public boolean isSubStructure(TreeNode A, TreeNode B) {
         return (A != null && B != null) && (recur(A, B) || isSubStructure(A.left, B) || isSubStructure(A.right, B));
     }
+
     boolean recur(TreeNode A, TreeNode B) {
-        if(B == null) return true;
-        if(A == null || A.val != B.val) return false;
+        if (B == null) return true;
+        if (A == null || A.val != B.val) return false;
         return recur(A.left, B.left) && recur(A.right, B.right);
     }
 
@@ -94,11 +97,12 @@ public class BinaryTreeRelatedQuestions {
      * 输出：[4,7,2,9,6,3,1]
      *
      * 递归法：考虑递归遍历（dfs）二叉树，交换每个节点的左 / 右子节点，即可生成二叉树的镜像。
+     *
      * @param root
      * @return
      */
     public TreeNode mirrorTree(TreeNode root) {
-        if(root == null) return null;
+        if (root == null) return null;
         TreeNode tmp = root.left;
         root.left = mirrorTree(root.right);
         root.right = mirrorTree(tmp);
@@ -107,21 +111,86 @@ public class BinaryTreeRelatedQuestions {
 
     /**
      * 非递归法：利用栈（或队列）遍历树的所有节点 nodenode ，并交换每个 nodenode 的左 / 右子节点。
+     *
      * @param root
      * @return
      */
     public TreeNode mirrorTree2(TreeNode root) {
-        if(root == null) return null;
-        Stack<TreeNode> stack = new Stack<>() {{ add(root); }};
-        while(!stack.isEmpty()) {
+        if (root == null) return null;
+        Stack<TreeNode> stack = new Stack<>() {{
+            add(root);
+        }};
+        while (!stack.isEmpty()) {
             TreeNode node = stack.pop();
-            if(node.left != null) stack.add(node.left);
-            if(node.right != null) stack.add(node.right);
+            if (node.left != null) stack.add(node.left);
+            if (node.right != null) stack.add(node.right);
             TreeNode tmp = node.left;
             node.left = node.right;
             node.right = tmp;
         }
         return root;
+    }
+
+
+    /**
+     * 剑指 Offer 28. 对称的二叉树
+     * 请实现一个函数，用来判断一棵二叉树是不是对称的。如果一棵二叉树和它的镜像一样，那么它是对称的。
+     * 例如，二叉树 [1,2,2,3,4,4,3] 是对称的。
+     *
+     * 1
+     * / \
+     * 2   2
+     * / \ / \
+     * 3  4 4  3
+     * 示例 1：
+     * 输入：root = [1,2,2,3,4,4,3]
+     * 输出：true
+     *
+     * @param root
+     * @return
+     */
+    public boolean isSymmetric(TreeNode root) {
+        return root == null ? true : recur2(root.left, root.right);
+    }
+
+    boolean recur2(TreeNode L, TreeNode R) {
+        if (L == null && R == null) return true;
+        if (L == null || R == null || L.val != R.val) return false;
+        return recur(L.left, R.right) && recur(L.right, R.left);
+    }
+
+
+    /**
+     * 剑指 Offer 55 - II. 平衡二叉树
+     * 输入一棵二叉树的根节点，判断该树是不是平衡二叉树。如果某二叉树中任意节点的左右子树的深度相差不超过1，那么它就是一棵平衡二叉树。
+     * 示例 1:
+     * 给定二叉树 [3,9,20,null,null,15,7]
+     *     3
+     *    / \
+     *   9  20
+     *     /  \
+     *    15   7
+     * 返回 true 。
+     *
+     * 解法：后序遍历 + 剪枝 （从底至顶）
+     * 思路是对二叉树做后序遍历，从底至顶返回子树深度，若判定某子树不是平衡树则 “剪枝” ，直接向上返回。
+     * 时间复杂度 O(N)O(N)： NN 为树的节点数；最差情况下，需要递归遍历树的所有节点。
+     * 空间复杂度 O(N)O(N)： 最差情况下（树退化为链表时），系统递归需要使用 O(N)O(N) 的栈空间。
+     *
+     * @param root
+     * @return
+     */
+    public boolean isBalanced(TreeNode root) {
+        return recur(root) != -1;
+    }
+
+    private int recur(TreeNode root) {
+        if (root == null) return 0;
+        int left = recur(root.left);
+        if(left == -1) return -1;
+        int right = recur(root.right);
+        if(right == -1) return -1;
+        return Math.abs(left - right) < 2 ? Math.max(left, right) + 1 : -1;
     }
 
 

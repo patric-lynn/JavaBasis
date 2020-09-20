4e9bbe4d33a0b99dd54fbacc065b77772767f3f1
lynn <358812977@163.com>
2020-09-20 21:07:44 +0800

    代码修改

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,40 @@
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Scanner;
-import java.util.stream.Stream;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void decode() {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        long p = 1000000007;
+        long[] result = new long[n];
+        for (int i = 0; i < n; i++) {
+            int a = in.nextInt();
+            int b = in.nextInt();
+            String op = in.next();
+            switch (op) {
+                case "+":
+                    result[i] = (a + b) % p;
+                    break;
+                case "-":
+                    result[i] = (a - b) % p;
+                    break;
+                case "*":
+                    result[i] = ((a % p) * (b % p)) % p;
+                    break;
+                case "^":
+                    long tmp = (long) (Math.pow(a, b));
+                    result[i] = tmp % p;
+                    break;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            System.out.println(result[i]);
+        }
+    }
 
+    public static void main(String[] args) {
+        decode();
     }
 }
-
-
-
 
 
 

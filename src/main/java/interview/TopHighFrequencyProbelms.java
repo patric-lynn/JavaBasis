3f5d13e22151c7ee70688b7eb15f1ccc7fa1ae88
patric-lynn <358812977@163.com>
2020-09-21 18:43:38 +0800

    update

--- src/main/java/interview/TopHighFrequencyProbelms.java
+++ src/main/java/interview/TopHighFrequencyProbelms.java
@@ -201,6 +201,30 @@ public class TopHighFrequencyProbelms {
         res[2][postIndex++] = root.val;
     }
 
+    /**
+     * 把一个数组最开始的若干个元素搬到数组的末尾，我们称之为数组的旋转。
+     * 输入一个非递减排序的数组的一个旋转，输出旋转数组的最小元素。
+     * 例如数组[3,4,5,1,2]为[1,2,3,4,5]的一个旋转，该数组的最小值为1。
+     *
+     * @param array
+     * @return
+     */
+    public int minNumberInRotateArray(int[] array) {
+        int start = 0;
+        int end = array.length - 1;
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (end - start == 1)
+                return array[end];
+            if (array[mid] >= array[start])
+                start = mid;
+            if (array[mid] <= array[end])
+                end = mid;
+        }
+        return -1;
+    }
+
+
     public static void main(String[] args) {
 
     }

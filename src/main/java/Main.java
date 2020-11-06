54cd7c944ecafbf248f6794d9c3b5fdddf0abf8e
lynn <358812977@163.com>
2020-11-06 16:37:49 +0800

    --story=增加二分查找多种方式

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,5 @@
-import java.util.Scanner;
+
+
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
-        String[] s;
-        int[] nums = new int[n];
-        s = in.next().split("");
-        for (int i = 0; i < n; i++) {
-            nums[i] = Integer.parseInt(s[i]);
-        }
-        System.out.println();
-    }
 }
--- src/main/java/basic/BinarySearch.java
+++ src/main/java/basic/BinarySearch.java
@@ -1,7 +1,5 @@
 package basic;
 
-import java.util.Arrays;
-
 /**
  * Created on 2020/9/7.
  *
@@ -9,31 +7,86 @@ import java.util.Arrays;
  */
 public class BinarySearch {
     public static void main(String[] args) {
-        int[] array = new int[]{8, 10, 11, 1, 3, 4, 7};
-        int p1 = 9;
-        int p2 = 3;
-        boolean b1 = isExist(array, p1);
-        boolean b2 = isExist(array, p2);
-        System.out.println(b1 + " " + b2);
+        int[] array = new int[]{1, 2, 3, 4, 5, 6, 7};
+        System.out.println(recursionBinarySearch(array, 2, 0, 6));
+        System.out.println(nonRecurBinarySearch(array, 4));
+        System.out.println(findBinarySearch(7, 3, array));
     }
 
-
-    public static boolean isExist(int[] array, int p) {
-        int temp = binarySearch(array, 0, array.length - 1, p);
-        if (temp == -1) return false;
-        return true;
-    }
-
-    public static int binarySearch(int[] array, int start, int end, int p) {
-        int middle = (start + end) / 2;
-        if (p == array[middle]) {
-            return middle;
-        }
-        if (array[middle] < array[start]) {
-            binarySearch(Arrays.copyOfRange(array,middle + 1, end), middle + 1, end, p);
-        } else if (array[middle] > array[end]) {
-            binarySearch(Arrays.copyOfRange(array, start, middle - 1), start, middle - 1, p);
+    /**
+     * 使用递归的二分查找
+     *
+     * @param array
+     * @param key
+     * @param low
+     * @param high
+     * @return
+     */
+    public static int recursionBinarySearch(int[] array, int key, int low, int high) {
+        if (low <= high) {
+            int mid = (low + high) >>> 1;
+            if (key < array[mid]) {
+                return recursionBinarySearch(array, key, low, mid - 1);
+            } else if (key > array[mid]) {
+                return recursionBinarySearch(array, key, mid + 1, high);
+            } else {
+                return mid;
+            }
         }
         return -1;
+    }
+
+
+    /**
+     * 不使用递归实现二分查找（while循环）
+     *
+     * @param array
+     * @param key
+     * @return
+     */
+    public static int nonRecurBinarySearch(int[] array, int key) {
+        int low = 0;
+        int high = array.length - 1;
+        while (low <= high) {
+            int mid = (low + high) >>> 1;
+            if (key < array[mid]) {
+                high = mid - 1;
+            } else if (key > array[mid]) {
+                low = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * 有条件二分查找：
+     * 请实现有重复数字的有序数组的二分查找，输出在数组中第一个大于等于查找值的位置，
+     * 如果数组中不存在这样的数，则输出数组长度加一
+     *
+     * @param n
+     * @param v
+     * @param array
+     * @return
+     */
+    public static int findBinarySearch(int n, int v, int[] array) {
+        if (n == 0)
+            return 1;
+        int left = 0;
+        int right = n - 1;
+        int index;
+        while (left <= right) {
+            index = (left + right) / 2;
+            if (array[index] >= v) {
+                if (index == 0 || array[index - 1] < v)
+                    return index + 1;
+                else
+                    right = index - 1;
+            } else {
+                left = index + 1;
+            }
+        }
+        return n + 1;
     }
 }
